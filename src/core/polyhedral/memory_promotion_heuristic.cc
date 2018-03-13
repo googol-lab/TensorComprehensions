6a1d4c389354719675a3af8b7a5ce17b9636c39e
Oleksandr Zinenko <git@ozinenko.com>
2018-03-13 16:41:24 +0100

    introduce a Kind of a promoted declaration
    A kernel variable can be declared as shared or as automatic (register).

--- src/core/polyhedral/memory_promotion_heuristic.cc
+++ src/core/polyhedral/memory_promotion_heuristic.cc
@@ -512,7 +512,8 @@ void promoteToSharedGreedy(
           continue;
         }
 
-        scop.promoteGroupToShared(
+        scop.promoteGroup(
+            Scop::PromotedDecl::Kind::SharedMem,
             tensorId,
             std::move(group),
             bandNode,
